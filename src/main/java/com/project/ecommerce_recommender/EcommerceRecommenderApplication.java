d8ae7580ad0cc09ba7311dbd8a333d6b8414e0a2
haryo <emadekuncoro@gmail.com>
2025-10-25 16:16:23 +0700

    initial commit

---
+++ src/main/java/com/project/ecommerce_recommender/EcommerceRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ecommerce_recommender/EcommerceRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
